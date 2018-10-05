b05b31590d7f909adb8119d22395912e37b001ae
MILAN\milan <milan_mico_stojanovski@hotmail.com>
2018-10-05 12:05:09 +0200

    Initial express server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://mikkac93:mikkac93@ds123513.mlab.com:23513/devconnector93'
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profiles = require('./routes/api/profiles');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Routes
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
+
