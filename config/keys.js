98181bdbcfd91e33ea34c480151237ea1878eb7a
MILAN\milan <milan_mico_stojanovski@hotmail.com>
2018-10-17 15:52:11 +0200

    'deploy_prepare'

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
-module.exports = {
-    mongoURI: 'mongodb://mikkac93:mikkac93@ds123513.mlab.com:23513/devconnector93',
-    secretOrKey: 'secret',
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+}
+else {
+    module.exports = require('./keys_dev');
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://mikkac93:mikkac93@ds123513.mlab.com:23513/devconnector93',
+    secretOrKey: 'secret',
+};
